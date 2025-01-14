15ec9fcb4e10b9bf2c02294aab4d47c34d040462
Ishika Knsal <ishikakasal05@gmail.com>
2025-01-14 19:21:22 +0530

    merger conflict solved

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-    build: {
-        chunkSizeWarningLimit: 1000, // Set the limit to 1000 KB (1 MB)
-      },
-=======
-  build: {
-    chunkSizeWarningLimit: 1000, // Set the limit to 1000 KB (1 MB)
-  },
->>>>>>> 296a04dee01447c41c9350cebbc9f035683f20c9
+build: {
+  chunkSizeWarningLimit: 1000, // Set the limit to 1000 KB (1 MB)
+},
   plugins: [react()],
 })
